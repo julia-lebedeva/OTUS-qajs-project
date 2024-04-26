94e0ecf15308810bb4b9c19370195ef61a3138a3
Yulia Lebedeva <yulia.lebedeva@jugru.org>
2024-04-26 21:14:44 +0300

    added timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  timeout: 60000,
+  expect: { timeout: 10000 },
+
   use: {
     baseURL: 'https://mobiusconf.com/',
 
